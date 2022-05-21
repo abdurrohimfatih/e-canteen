ce4e096a1ac7494ea3f8a675ed967fc80ecfbea9
abdurrohimfatih <rohimohim7@gmail.com>
2022-05-21 10:00:08 +0700

    Fixing 43

--- src/main/java/com/ecanteen/ecanteen/utils/MySQLConnection.java
+++ src/main/java/com/ecanteen/ecanteen/utils/MySQLConnection.java
@@ -6,12 +6,23 @@ import java.sql.SQLException;
 
 public class MySQLConnection {
     private static final String URL = "jdbc:mysql://192.168.1.1:3306/ecanteen";
+    private static final String URL2 = "jdbc:mysql://localhost:3306/ecanteen";
     private static final String USERNAME = "ecanteen";
+    private static final String USERNAME2 = "root";
     private static final String PASSWORD = "idc2022";
+    private static final String PASSWORD2 = "";
 
     public static Connection createConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            connection = DriverManager.getConnection(URL2, USERNAME2, PASSWORD2);
+        }
+
         connection.setAutoCommit(false);
         return connection;
     }
